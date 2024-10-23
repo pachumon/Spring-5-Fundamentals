57c345b0fae2bba38f8d98965dd02550b384d28d
pachumon <shenoy.prasanth@gmail.com>
2024-10-23 10:38:26 +0000

    java configuration setter injection

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,12 +2,15 @@ package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        SpeakerService service= new SpeakerServiceImpl();
+        ApplicationContext appContext= new AnnotationConfigApplicationContext(AppConfig.class);
+        SpeakerService service= appContext.getBean("speakerService",SpeakerService.class);
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
