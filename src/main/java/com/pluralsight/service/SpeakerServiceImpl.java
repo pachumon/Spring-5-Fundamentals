91c94a91ea2c1892758dd7d13492db5d4db1f1b3
pachumon <shenoy.prasanth@gmail.com>
2024-10-24 05:32:40 +0000

    adding stereotype annotation with componentsca  java configuration

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,13 +3,22 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(){
+        System.out.println("SpeakerServiceImpl repository constructor");
+    }
     public SpeakerServiceImpl(SpeakerRepository speakerRepository){
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -18,7 +27,9 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 }
